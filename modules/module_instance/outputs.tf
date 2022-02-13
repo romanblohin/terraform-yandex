2b14b327279a45c384b5440a6f45eb74f8f27e4e
Roman Blohin <blohin_roman@mail.ru>
2022-02-13 15:22:42 +0000

    created Load Balancer

--- modules/module_instance/outputs.tf
+++ modules/module_instance/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
-  value = yandex_compute_instance.default.network_interface.*.nat_ip_address
+  value = yandex_compute_instance.default.network_interface[0].nat_ip_address
+}
+
+output "internal_ip" {
+  value = yandex_compute_instance.default.network_interface[0].ip_address
 }
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "lamp_public_ip" {
   description = "Public Ipv4 address of LAMP server"
   value       = module.instance_lamp.public_ip
 }
+
+output "lb_public_ip" {
+   description = "Load Balancer Public IP"
+   value       = data.yandex_lb_network_load_balancer.foo.listener
+}
